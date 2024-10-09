e58fac987d3951995a0744bfdb355b70bdcaa422
danielehc <daniele@hashicorp.com>
2024-10-09 16:39:13 +0200

    Tune load test script

--- shared/jobs/05.load-test.sh
+++ shared/jobs/05.load-test.sh
@@ -3,18 +3,29 @@
 # This script requires the hey tool
 # https://github.com/rakyll/hey
 
-URL=$1
-echo "Application address: $1"
+[ -z "$1" ] && echo "No URL passed as first argument...exiting" && exit 1
 
-for i in $(seq 1 5);
+_URL=$1
+echo "Application address: $_URL"
+
+_waves=5
+
+_wave_duration=15
+_workers_multiplier=7
+_rate_multiplier=6
+_sleep_time=7
+
+
+for i in $(seq 1 $_waves);
 do
-    DURATION=15
-    CONCURRENT_WORKERS=$((7*$i))
-    RATE_LIMIT_PER_SEC_PER_WORKER=$((6*$i))
-    echo "Sending `expr $DURATION \* $CONCURRENT_WORKERS \* $RATE_LIMIT_PER_SEC_PER_WORKER` requests over $DURATION seconds"
-    hey -z "$DURATION"s -c $CONCURRENT_WORKERS -q $RATE_LIMIT_PER_SEC_PER_WORKER -m GET $URL > /dev/null
+    _wave_duration=15
+    _concurrent_workers=$(($_workers_multiplier * $i))
+    _rate_limit_per_sec_per_worker=$(($_rate_multiplier * $i))
 
-    SLEEP=7
-    echo "Waiting $SLEEP seconds..."
-    sleep $SLEEP
+    echo "Sending $(($_wave_duration * $_concurrent_workers * $_rate_limit_per_sec_per_worker)) requests over $_wave_duration seconds"
+
+    hey -z "$_wave_duration"s -c $_concurrent_workers -q $_rate_limit_per_sec_per_worker -m GET ${_URL} > /dev/null
+
+    echo "Waiting $_sleep_time seconds..."
+    sleep $_sleep_time
 done
